fccdcebf97261a42cfd3dbb285099b7102f4901b
host <cloftus@princeton.edu>
2022-10-23 15:47:24 -0400

    fastapi api for starting/stopping server

--- server/server.py
+++ server/server.py
@@ -1,12 +1,12 @@
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 import uvicorn
+import os, signal, dbm
 import multiprocessing as mp
 from typing import List, Tuple
 
 import flwr as fl
 from flwr.common import Metrics
-
 
 # Define metric aggregation function
 def weighted_average(metrics: List[Tuple[int, Metrics]]) -> Metrics:
@@ -23,9 +23,20 @@ strategy = fl.server.strategy.FedAvg(
     evaluate_metrics_aggregation_fn=weighted_average)
 
 
-
-
 app = FastAPI()
+
+# on startup, start the database
+@app.on_event("startup")
+async def startup_event():
+    global db
+    db = dbm.open('db', 'c')
+
+# on shutdown, close the database and delete ephemeral files
+@app.on_event("shutdown")
+async def shutdown_event():
+    for key in db.keys():
+        del db[key]
+    db.close()
 
 origins = [
     "http://localhost:5000",
@@ -39,48 +50,44 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
-
-SERVER_RUNNING = False
+def start_server():
+    fl.server.start_server(
+        server_address="0.0.0.0:8080",
+        config=fl.server.ServerConfig(num_rounds=3),
+        strategy=strategy,
+    )
 
 @app.get("/")
-async def read_root():
+def read_root():
     return {"Hello": "World"}
 
-@app.get("/start_server")
+@app.get("/pid")
+async def get_pid():
+    if db.get(b"server_running") == b'1':
+        return {"pid": db.get("pid")}
+    return {'pid': "Null"}
+
+@app.get("/start")
 async def start_server():
     # check if server is already 
-    if SERVER_RUNNING:
+    if db.get(b'server_running') == b'1':
         return {"message": "Server already running"}
     else:
-        SERVER_RUNNING = True
-        def start_server():
-            fl.server.start_server(
-                server_address="0.0.0.0:8080",
-                config=fl.server.ServerConfig(num_rounds=3),
-                strategy=strategy,
-            )
         p = mp.Process(target=start_server)
         p.start()
+        db[b'pid'] = str(p.pid).encode('utf-8')
+        db[b'server_running'] = b'1'
         return {"message": "Server started"}
 
-@app.get("/stop_server")
+@app.get("/stop")
 async def stop_server():
-    if SERVER_RUNNING:
-        SERVER_RUNNING = False
-        fl.server.
+    if db.get(b'server_running') == b'1':
+        pid = int(db[b'pid'])
+        os.kill(pid, signal.SIGKILL)
+        db[b'server_running'] = b'0'
         return {"message": "Server stopped"}
     else:
         return {"message": "Server not running"}
-
-
-@app.get('/profile')
-def my_profile():
-    response_body = {
-        "name": "Colton",
-        "about" :"Hello! I'm a full stack developer that loves python and javascript"
-    }
-
-    return response_body
 
 
 if __name__ == "__main__":
